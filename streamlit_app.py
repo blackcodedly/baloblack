1b6d26125a6d158ebe708ea9e5f9366b2c0a1267
blackcodedly <90612685+blackcodedly@users.noreply.github.com>
2024-01-26 19:01:48 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,14 @@
 import streamlit as st
 from PIL import Image
 
-# Custom function to create and display buttons in Streamlit using Markdown and HTML
 def st_button(button_text, url, tooltip_text, icon_size):
+    # Updated button HTML to change the background color to blue and ensure the text is centered
     button_html = f"""<a href="{url}" target="_blank" style="text-decoration: none;">
-    <button style="background-color: #4CAF50; border: none; color: white; padding: 10px {icon_size}px; text-align: center;
-    text-decoration: none; display: inline-block; font-size: 16px; margin: 4px 2px; cursor: pointer; border-radius: 12px;">
-    {button_text} <span style="font-size: {icon_size}px;">✍️🤖</span>
+    <button style="background-color: #007BFF; border: none; color: white; padding: 10px 20px; text-align: center;
+    text-decoration: none; display: block; font-size: 16px; margin: 4px auto; cursor: pointer; border-radius: 12px; width: 100%;">
+    {button_text}
     </button></a>"""
     st.markdown(button_html, unsafe_allow_html=True)
-
-# Assuming load_css properly injects custom CSS into your Streamlit app
-# load_css() # Uncomment this line if your load_css function is defined elsewhere
 
 # Display a GitHub stars badge using markdown
 st.markdown("[![Star](https://img.shields.io/github/stars/dataprofessor/links.svg?logo=github&style=social)](https://gitHub.com/dataprofessor/links)", unsafe_allow_html=True)
@@ -25,9 +22,9 @@ st.header('Sulaiman.')
 
 st.info('A proficient expert leveraging advanced AI tools like ChatGPT to offer a variety of online services')
 
-icon_size = 20  # Adjusts padding around the button text
+icon_size = 20  # This no longer affects padding directly due to CSS adjustments
 
-# Replacing previous st_button function calls with the updated implementation
+# Updated st_button function calls
 st_button('Follow me on Twitter', 'https://twitter.com/baloblack/', 'Follow me on Twitter', icon_size)
 st_button('Follow me on LinkedIn', 'https://www.linkedin.com/in/sulaiman-balogun-208557175/', 'Follow me on LinkedIn', icon_size)
 st_button('Buy me a Coffee', 'https://www.buymeacoffee.com/baloblack/', 'Support me on Buy me a Coffee', icon_size)
