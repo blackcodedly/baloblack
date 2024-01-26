f6a4b042bf52a49c8eac73b369a710cad6d61e9c
blackcodedly <90612685+blackcodedly@users.noreply.github.com>
2024-01-26 18:35:09 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,9 @@ st.info('A proficient expert leveraging advanced AI tools like ChatGPT to offer 
 
 icon_size = 20
 
-st.info('A proficient expert leveraging advanced AI tools like ChatGPT to offer a variety of online services')
-
 st_button('twitter', 'https://twitter.com/baloblack/', 'Follow me on Twitter', icon_size)
 st_button('linkedin', 'https://www.linkedin.com/in/sulaiman-balogun-208557175//', 'Follow me on LinkedIn', icon_size)
 st_button('cup', 'https://www.buymeacoffee.com/baloblack/', 'Buy me a Coffee', icon_size)
+
+st.info('Check out my GPT-powered applications
+st_button('Copywrite GPT', 'https://chat.openai.com/g/g-bFeD4GzK7-copywritegpt-crafting-ai-driven-content/', 'Crafting AI-Driven Content ✍️🤖 Revolutionizing Your Brands.', icon_size)
