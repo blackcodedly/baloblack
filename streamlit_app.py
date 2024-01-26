af7b772309df855ea4cfe0a869eec369edcdf096
blackcodedly <90612685+blackcodedly@users.noreply.github.com>
2024-01-26 18:30:25 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,8 @@ st.info('A proficient expert leveraging advanced AI tools like ChatGPT to offer 
 
 icon_size = 20
 
+st.info('A proficient expert leveraging advanced AI tools like ChatGPT to offer a variety of online services')
+
 st_button('twitter', 'https://twitter.com/baloblack/', 'Follow me on Twitter', icon_size)
 st_button('linkedin', 'https://www.linkedin.com/in/sulaiman-balogun-208557175//', 'Follow me on LinkedIn', icon_size)
 st_button('cup', 'https://www.buymeacoffee.com/baloblack/', 'Buy me a Coffee', icon_size)
